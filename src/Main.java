8cda8aad110265f72dce10eef9b909180e7e6241
Edinorock <111507622+Edinorock@users.noreply.github.com>
2022-12-05 13:15:22 +0300

    Hw products map

--- src/Main.java
+++ src/Main.java
@@ -10,34 +10,15 @@ public class Main {
         Product fish = new Product("Рыба", 500, 2);
         Product salt = new Product("Соль", 50, 1);
 
-        ProductList productList1 = new ProductList("Список продуктов №1");
-        productList1.addProduct(salt, fish);
-        productList1.checkProduct("Соль");
-        productList1.removeProduct(fish);
-        System.out.println(productList1);
+        // ProductList productList1 = new ProductList("Список продуктов №1");
+        // productList1.addProduct(salt, fish);
+        //  productList1.checkProduct("Соль");
+        //  productList1.removeProduct(fish);
+        //  System.out.println(productList1);
 
-        Recipe recipe1 = new Recipe("Запеченная рыба", salt);
-        recipe1.addProductList(productList1);
-        System.out.println(recipe1);
+        //  Recipe recipe1 = new Recipe("Запеченная рыба", salt);
+        //  recipe1.addProductList(productList1);
+        //  System.out.println(recipe1);
 
-        numberSetRemoveEven();
-    }
-
-    private static void numberSetRemoveEven() {
-        Set<Integer> numbers = new HashSet<>();
-        Random random = new Random();
-        for (int i = 0; i < 20; i++) {
-            numbers.add(random.nextInt(0));
-        }
-
-        System.out.println(numbers);
-        Iterator<Integer> iterator = numbers.iterator();
-        while (iterator.hasNext()) {
-            Integer next = iterator.next();
-            if (next % 2 != 0) {
-                iterator.remove();
-            }
-        }
-        System.out.println(numbers);
     }
 }
--- src/Recipe.java
+++ src/Recipe.java
@@ -1,10 +1,11 @@
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import java.util.Map;
+import java.util.HashMap;
 public class Recipe {
     public final static Set<Recipe> allRecipes = new HashSet<>();
-    private final Set<Product> productsSet = new HashSet<>();
+    private final Map<Product, Integer> productsSet = new HashMap<>();
     private final String recipeName;
 
     public Recipe(String recipeName, Product... products) {
@@ -13,23 +14,23 @@ public class Recipe {
         allRecipes.add(this);
     }
 
-    public void addProductList(ProductList productList) {
-        productsSet.addAll(productList.getProductsSet());
-    }
+    // public void addProductList(ProductList productList) {
+    //  productsSet.addAll(productList.getProductsSet());
+    // }
 
     public void addProduct(Product... products) {
-        Collections.addAll(productsSet, products);
-
+        this.addProduct(1, products);
     }
 
-    public Set<Product> getProductsSet() {
-        return productsSet;
+    public void addProduct(int count, Product... products) {
+        for (Product product : products) {
+            this.productsSet.put(product, count);
+        }
     }
-
     public int getTotalPrice() {
         int sum = 0;
-        for (Product product : productsSet) {
-            sum += product.getPrice();
+        for (Map.Entry<Product, Integer> entry : productsSet.entrySet()) {
+            sum += entry.getKey().getPrice() * entry.getValue();
         }
         return sum;
     }
@@ -38,22 +39,13 @@ public class Recipe {
         return recipeName;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) {
-            return false;
+    public String getProductsSet() {
+        StringBuilder builder = new StringBuilder();
+        for (Map.Entry<Product, Integer> entry : this.productsSet.entrySet()) {
+            builder.append(entry.getKey()).append(" --> ").append(entry.getValue()).append(",\n");
         }
-        Recipe recipe = (Recipe) o;
-        if (Objects.equals(recipeName, recipe.recipeName)) {
-            throw new RuntimeException("Такой рецепт уже существует");
-        }
-        return false;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(recipeName);
+        builder.append("TotalPrice = " + getTotalPrice() + " ,");
+        return String.valueOf(builder);
     }
 
     @Override
@@ -64,3 +56,4 @@ public class Recipe {
                 '}';
     }
 }
+
